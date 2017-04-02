7c1e1946f50baec62056bd73c4ea36f237b95bc2
Ram <rammurat.rms.sharma0@gmail.com>
2017-04-02 07:39:42 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    appId: '<your flock app id>',
+    appSecret: '<your flock app secret>',
+    botUserId:'<your bot user id>',
+    botToken:'<your bot token>',
+    witToken:'<your secret app wit token>',
+    exportXLS : true, //default true, this should updated to false once you setup your code 
+    publicDir : "public",
+    nodeDir : "node_modules",
+    port:8080
+};
